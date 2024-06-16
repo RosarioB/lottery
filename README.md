b8801049fce5008ed57c318e2e60fb94135d29d0
Rosario Borgesi <borgesiros@gmail.com>
2024-06-16 15:14:58 +0200

    Updated README and react application

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lottery
+A simple Ethereum application that simulates a lottery consisting of:
+- A lottery smart contract acting as the back-end in the folder `smart contract`.
+- A front-end application in React that interacts with the smart contract, using the web3.js library, in the folder `react`.
+
+![alt text](https://github.com/RosarioB/lottery/blob/main/github_images/lottery.png?raw=true)
+
+1. Add a .env file in the root of the project smart_contract with `PRIVATE_KEY` and `INFURA_URL`
+2. Run `npm install` on both the folders `smart contract` and `react`
+3. Compile and deploy the smart contract: `node deploy.js`
+4. Run the React application `npm start`
---
+++ react/src/lottery.js
@@ -0,0 +1,94 @@
+import web3 from "./web3";
+
+const address = '0xdF3Aa4D8845ad3537F989e7fEebb5E451F1d49EB';
+
+const abi =  
+[
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "manager",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "pickWinner",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getPlayers",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "enter",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "players",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "lastWinner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    }
+  ]
+
+export default new web3.eth.Contract(abi, address);
